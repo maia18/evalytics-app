cae0e1033773e0141fd1b7216e980b3186299954
maia18 <nelsonkaue05100@gmail.com>
2026-07-10 19:35:40 -0300

    ..

---
+++ config/firebase_config.py
@@ -0,0 +1,13 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+try:
+    cred = credentials.Certificate("database/credentials.json")
+    
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred)
+        
+    db = firestore.client()
+    
+except Exception as e:
+    print(f"Erro Crítico na Conexão: {e}")
