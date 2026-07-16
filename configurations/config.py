adf3f57833495651d9001c515ad7768a383c2cb9
maia18 <nelsonkaue05100@gmail.com>
2026-07-16 12:28:11 -0300

    new gitignore

--- configurations/config.py
+++
@@ -1,8 +0,0 @@
-# Título da aplicação (aparece na janela da interface)
-APP_TITLE = "Evalytics - Avaliação Institucional"
-
-# Largura da janela principal da aplicação (em pixels)
-WINDOW_WIDTH = 1280
-
-# Altura da janela principal da aplicação (em pixels)
-WINDOW_HEIGHT = 720
