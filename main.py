a9cc8dfa9276ef14a3e4f3187d9093b60c874851
maia18 <nelsonkaue05100@gmail.com>
2026-08-11 15:19:08 -0300

    new main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import logging as lg # módulo nativo do Python usado para registrar mensagens, 
 from components.core.globals import configurar_aplicacao # configurações visuais da página (título, tema, tamanho da janela, etc).
 from components.core.navigator import Navigator # classe customizada criada para gerenciar o roteamento/troca de telas no app.
 
-logger = lg.getLogger(__name__) # Cria uma instância de logger específica para este arquivo. Usar __name__ ajuda a identificar nos logs exatamente de qual módulo (arquivo) a mensagem veio.
-ROTA_INICIAL = "/" # Define uma constante para a rota inicial. O padrão "/" geralmente aponta para a Home ou tela de Login.
+'''
+Cria uma instância de logger específica para este arquivo. 
+    Usar __name__ ajuda a identificar nos logs exatamente de qual módulo (arquivo) a mensagem veio.
+'''
+logger = lg.getLogger(__name__) 
+
+ROTA_INICIAL = "/" # Define uma constante para a rota inicial.
 
 def main(page: ft.Page) -> None:
     """
@@ -16,9 +21,8 @@ def main(page: ft.Page) -> None:
         Navigator(page).go(ROTA_INICIAL) # Instancia a classe de navegação passando a página atual, e envia o usuário para a primeira tela.
         
     except Exception:
-        logger.exception("Falha ao inicializar a aplicação.") # Se qualquer erro crítico acontecer na montagem da tela inicial, o logger registra o erro completo (com o traceback).
-        
-        raise # Propaga o erro para frente, forçando o programa a parar. Isso evita que o app abra quebrado e oculte o problema (fail-fast).
+        logger.exception("Falha ao inicializar a aplicação.") # Se qualquer erro crítico acontecer na montagem da tela inicial, o logger registra o erro completo (com o traceback)
+        raise # Propaga o erro para frente, forçando o programa a parar
 
 if __name__ == "__main__":
     lg.basicConfig(level=lg.INFO) # Configura o sistema de logs para exibir mensagens a partir do nível INFO (ignora mensagens de DEBUG, mas mostra avisos e erros).
